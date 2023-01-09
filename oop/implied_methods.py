ce1ba533b9b4f6d6b6da92c57060ec28dbe8f549
Edwin Jaico-Berg <e.j.berg@outlook.com>
2023-01-09 13:55:51 +0000

    Update

--- oop/implied_methods.py
+++ oop/implied_methods.py
@@ -3,7 +3,11 @@ class Vector:
 
     def __init__(self, d):
         """Create d-dimensional vector of zeros."""
-        self._coords = [0] * d
+        # self._coords = [0] * d
+        if type(d) == int:
+            self._coords = [d]
+        elif type(d) == list:
+            self._coords = d
 
     def __len__(self):
         """Return the dimension of the vector."""
@@ -26,6 +30,37 @@ class Vector:
             result[i] = self[i] + other[i]
         return result
 
+    def __radd__(self, other):
+        """Return the sum of a list and a vector."""
+        if len(other) != len(self):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for i in range(len(self)):
+            result[i] = self[i] + other[i]
+        return result
+
+    def __mul__(self, other):
+        """Return the multiple of vector."""
+        if type(other) == int:
+            result = Vector(len(self))
+            for i in range(len(self)):
+                result[i] = self[i] * other
+            return result
+        elif type(other) == Vector:
+            if len(self) != len(other):
+                raise ValueError("dimensions must agree")
+            result = Vector(len(self))
+            for i in range(len(self)):
+                result[i] = self[i] * other[i]
+            return result
+
+    def __rmul__(self, other):
+        """Return the multiple of vector."""
+        result = Vector(len(self))
+        for i in range(len(self)):
+            result[i] = self[i] * other
+        return result
+
     def __eq__(self, other):
         """Return True if vector has same coordinates as other."""
         return self._coords == other._coords
@@ -36,7 +71,7 @@ class Vector:
 
     def __str__(self):
         """Produce string representation of vector."""
-        return f'<{str(self._coords)}>' # adapt list representation
+        return f'<{str(self._coords)[1:-1]}>' # adapt list representation
 
 
 if __name__ == "__main__":
@@ -50,4 +85,9 @@ if __name__ == "__main__":
     for entry in v:
         total += entry
     print(total)
-    
+    u = v + [5, 3, 10, -2, 1]
+    u = [5, 3, 10, -2, 1] + v   # doesn't run as no __radd__
+    print(u)
+    print(v * 3)
+    print(3 * v)
+    print(v * u)
