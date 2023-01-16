879b7338774954017e0e60d37c38a8e06f00afde
Edwin Jaico-Berg <e.j.berg@outlook.com>
2023-01-16 11:03:03 +0000

    Added gitignore

--- array-based-sequences/constructing_multidimensional_arrays.py
+++ array-based-sequences/constructing_multidimensional_arrays.py
@@ -11,7 +11,7 @@ def incorrect_multidimensional_array_construction():
 def correct_multidimensional_array_construction():
     r = 3
     c = 3
-    data = [[0] * c for j in range(r)]
+    data = [[0] * c for _ in range(r)]
 
     print(data)
     data[2][0] = 100
