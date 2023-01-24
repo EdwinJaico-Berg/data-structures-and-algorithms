61a142be68b23a5f46e3242dea55823b6e8dc6ef
Edwin Jaico-Berg <e.j.berg@outlook.com>
2023-01-24 11:22:39 +0000

    Sorted priority queue

--- 9-priority-queues/unsorted_priority_queue.py
+++ 9-priority-queues/unsorted_priority_queue.py
@@ -20,3 +20,27 @@ class UnsortedPriorityQueue(PriorityQueueBase):
                 small = walk
             walk = self._data.after(walk)
         return small
+
+    def __init__(self):
+        """Create a new empty Priority Queue"""
+        self._data = PositionalList()
+
+    def __len__(self):
+        """Return the number of items in the priority queue."""
+        return len(self._data)
+
+    def add(self, key, value):
+        """Add a key-value pair."""
+        self._data._add_last(self._Item(key, value))
+
+    def min(self):
+        """Return but do not remove (k,v) tuple with minimum key."""
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+
+    def remove_min(self):
+        """Remove and return (k,v) tuple with minimum key."""
+        p = self._find_min()
+        item = self._data.delete(p)
+        return (item._key, item._value)
